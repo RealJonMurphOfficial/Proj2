ac5b95d1099f1526ac9ab8aed0406fe3fcd35642
RealJonMurphOfficial <jonathan.p.murphy49@gmail.com>
2024-02-14 10:09:22 -0500

    Tester and starting framework

--- src/SelectBox.java
+++ src/SelectBox.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Placeholder comment.
+ * Add your name under author after working on this class.
+ * @author Jonathan Murphy,
+ */
 public class SelectBox {
+    //Attributes
+    private boolean selected;
+    private int id;
+    private JButton button;
+    //Methods
 }
