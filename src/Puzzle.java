c00949db8fbc1084229ad60d1a9129366f8c132b
RealJonMurphOfficial <jonathan.p.murphy49@gmail.com>
2024-02-20 11:20:23 -0500

    start on Puzzle

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
  * Add your name under author after working on this class.
  * @author Jonathan Murphy,
  */
-public class Puzzle {
+public abstract class Puzzle {
     //Attributes
     private int categories, items;
     private String clues;
     private ArrayList<PuzGrid> grids;
     private ArrayList<String> hints;
     //Methods
+    public Puzzle(int nCat, int nItem) {
+        //
+    }
+    public String getClues() {
+        return clues;
+    }
+    public ArrayList<PuzGrid> getGrids() {
+        return grids;
+    }
+    public String showHint() {
+        return "take remove hint from hints";
+    }
+    public void setClues(String cList) {
+        clues = cList;
+    }
+    public void addGrid(PuzGrid grid) {
+        //
+    }
+    public void addHint(String hint) {
+        //
+    }
+    public abstract boolean checkSelect();
 }
