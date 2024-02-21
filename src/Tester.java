42b3df7c0d449d12fed0e5a48d82cc7a53d02e46
SalimJday <144251605+SalimJday@users.noreply.github.com>
2024-02-21 18:45:36 -0500

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class Tester {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Sample Puzzle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Puzzle puzzle = new SamplePuzzle();
+        PuzPane pane = new PuzPane(puzzle);
+
+        frame.getContentPane().add(pane);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
