1ff9975b4bd7518e6d04df9a65c870466e55216d
SalimJday <144251605+SalimJday@users.noreply.github.com>
2024-02-21 18:45:02 -0500

    Update Puzzle.java

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,2 +1,53 @@
-public class Puzzle {
+import java.util.ArrayList;
+
+public abstract class Puzzle {
+    //Attributes
+    private int categories, items;
+    private String clues;
+    private ArrayList<PuzGrid> grids;
+    private ArrayList<String> hints;
+
+    //Constructor
+    public Puzzle(int nCat, int nItem) {
+        categories = nCat;
+        items = nItem;
+        clues = "";
+        grids = new ArrayList<>();
+        hints = new ArrayList<>();
+    }
+
+    public String getClues() {
+        return clues;
+    }
+
+    public int getItems() {
+        return items;
+    }
+
+    public ArrayList<PuzGrid> getGrids() {
+        return grids;
+    }
+
+    public void setClues(String cList) {
+        clues = cList;
+    }
+
+    public void addGrid(PuzGrid grid) {
+        grids.add(grid);
+    }
+
+    public void addHint(String hint) {
+        hints.add(hint);
+    }
+
+    public abstract boolean checkSelect();
+
+    public String showHint() {
+        if (hints.isEmpty()) {
+            return "No hints available.";
+        }
+        String hint = hints.get(0);
+        hints.remove(0);
+        return hint;
+    }
 }
