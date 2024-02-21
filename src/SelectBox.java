8604e84b8243a88740f7d264b9608c4d865429f4
SalimJday <144251605+SalimJday@users.noreply.github.com>
2024-02-21 18:45:27 -0500

    Update SelectBox.java

--- src/SelectBox.java
+++ src/SelectBox.java
@@ -1,2 +1,46 @@
-public class SelectBox {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class SelectBox extends JPanel {
+    //Attributes
+    private boolean selected;
+    private int id;
+    private JButton button;
+
+    //Constructor
+    public SelectBox(int i) {
+        selected = false;
+        id = i;
+
+        button = new JButton();
+        button.setBackground(Color.WHITE);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selected = !selected;
+                button.setBackground(selected ? Color.GREEN : Color.WHITE);
+            }
+        });
+
+        add(button);
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public JButton getButton() {
+        return button;
+    }
+
+    public void toggle() {
+        selected = !selected;
+        button.setBackground(selected ? Color.GREEN : Color.WHITE);
+    }
 }
