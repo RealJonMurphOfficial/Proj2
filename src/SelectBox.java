2f42a1f60b0ebcf5c2067240d7eba7bcb5740a24
RealJonMurphOfficial <jonathan.p.murphy49@gmail.com>
2024-02-22 17:53:16 -0500

    SelectBox equals toString

--- src/SelectBox.java
+++ src/SelectBox.java
@@ -33,4 +33,24 @@ public class SelectBox {
         button.setBackground(selected ? Color.GREEN : Color.WHITE);
         return selected;
     }
+    @Override
+    public boolean equals(Object o) {
+        if(super.equals(o) && getClass() == o.getClass()) {
+            if(selected == ((SelectBox) o).isSelected() && id == ((SelectBox) o).getId() && button.equals(((SelectBox) o).getButton())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public String toString() {
+        String result = "SelectBox ID: " + id;
+        if(selected) {
+            result += ", selected";
+        }
+        else {
+            result += ", not selected";
+        }
+        return result;
+    }
 }
