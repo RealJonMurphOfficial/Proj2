a0405d8f166e778ce6e218df9010d108f4a0a4bd
ian chung <130524551+2annnnnnnn@users.noreply.github.com>
2024-02-24 15:47:20 -0500

    Update Puzzle.java

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -2,26 +2,27 @@ import java.util.ArrayList;
 /**
  * Placeholder comment.
  * Add your name under author after working on this class.
- * @author Jonathan Murphy, Ian Chung
+ * @author Jonathan Murphy, Ian Chung, Salim Jday
  */
 public abstract class Puzzle {
     //Attributes
     private int categories, items;
-    private ArrayList<String> clues;
+    private String clues;
     private ArrayList<PuzGrid> grids;
     private ArrayList<String> hints;
     //Methods
     public Puzzle(int nCat, int nItem) {
-        //set nCat to categories and nItem to items
-        this.categories = nCat;
-        this.items = nItem;
-        clues = new ArrayList<>();
+        categories = nCat;
+        items = nItem;
         grids = new ArrayList<>();
         hints = new ArrayList<>();
     }
     //getters and setters for use by subclasses
-    public ArrayList<String> getClues() {
+    public String getClues() {
         return clues;
+    }
+    public int getCategories() {
+        return categories;
     }
     public int getItems() {
         return items;
@@ -30,26 +31,47 @@ public abstract class Puzzle {
         return grids;
     }
     public String showHint() {
-        return "take remove hint from hints";
+        if (hints.isEmpty()) {
+            return "No hints available.";
+        }
+        String hint = hints.get(0);
+        hints.remove(0);
+        return hint;
     }
-    public void setClues(String clue) {
-        clues.add(clue);
+    public void setClues(String cList) {
+        clues = cList;
     }
     public void addGrid(PuzGrid grid) {
-        //add grid to grids
         grids.add(grid);
     }
     public void addHint(String hint) {
-        //add hint to hints
         hints.add(hint);
     }
     public abstract boolean checkSelect();
-  /*
-    public boolean checkSelect(){
-      for (PuzGrid grid: grids){
-          if (!grid.checkSelect()){
-              return false;
-          }
-      }
-  */
+    /**
+     * Compares this Object to another.
+     * @param o Object to be compared
+     * @return true if Object o is the same as this Object
+     */
+    @Override
+    public boolean equals(Object o) {
+        if(super.equals(o) && getClass() == o.getClass()) {
+            if(categories == ((Puzzle) o).getCategories() && items == ((Puzzle) o).getItems() && clues.equals(((Puzzle) o).getClues()) && grids.equals(((Puzzle) o).getGrids())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Returns a String containing information regarding the object's attributes.
+     * @return String listing object's key attributes
+     */
+    @Override
+    public String toString() {
+        String result = categories + " X " + items + " puzzle.\nContains Grids:";
+        for(PuzGrid grid : grids) {
+            result += "\n" + grid.toString();
+        }
+        return result;
+    }
 }
