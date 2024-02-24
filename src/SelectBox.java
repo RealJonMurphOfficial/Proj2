b21191c0b8c8de10bce1800e9d5d45ebd45b1151
ian chung <130524551+2annnnnnnn@users.noreply.github.com>
2024-02-24 15:46:15 -0500

    Update SelectBox.java

--- src/SelectBox.java
+++ src/SelectBox.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 /**
- * Placeholder comment.
- * Add your name under author after working on this class.
+ * Represents the selectable box of the puzzle grid.
+ * Allows toggling the selection and updateing the button's color accordingly.
  * @author Jonathan Murphy, Salim Jday, Ian Chung
  */
 public class SelectBox {
@@ -19,6 +19,7 @@ public class SelectBox {
         id = i;
         button = new JButton("" + i);
     }
+    //Getters
     public boolean isSelected() {
         return selected;
     }
@@ -28,9 +29,39 @@ public class SelectBox {
     public JButton getButton() {
         return button;
     }
+    //Toggles and updates the button color.
     public boolean toggle() {
         selected = !selected;
         button.setBackground(selected ? Color.GREEN : Color.WHITE);
         return selected;
+    }
+    /**
+     * Compares this Object to another.
+     * @param o Object to be compared
+     * @return true if Object o is the same as this Object
+     */
+    @Override
+    public boolean equals(Object o) {
+        if(super.equals(o) && getClass() == o.getClass()) {
+            if(selected == ((SelectBox) o).isSelected() && id == ((SelectBox) o).getId() && button.equals(((SelectBox) o).getButton())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Returns a String containing information regarding the object's attributes.
+     * @return String listing object's key attributes
+     */
+    @Override
+    public String toString() {
+        String result = "SelectBox ID: " + id;
+        if(selected) {
+            result += ", selected";
+        }
+        else {
+            result += ", not selected";
+        }
+        return result;
     }
 }
