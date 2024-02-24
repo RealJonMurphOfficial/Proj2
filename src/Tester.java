14d7a748f0b7153f63ce37bb0f288ac724bef3b4
ian chung <130524551+2annnnnnnn@users.noreply.github.com>
2024-02-24 15:48:11 -0500

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+/**
+ * Tester class/main method for PuzPane and Puzzle classes
+ * Displays an instance of PuzPane containing SamplePuzzle on a JFrame.
+ * @author Jonathan Murphy, Salim Jday,
+ * @version 1.0
+ */
 public class Tester {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Sample Puzzle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Puzzle puzzle = new SamplePuzzle();
+        PuzPane pane = new PuzPane(puzzle);
+
+        frame.getContentPane().add(pane);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
